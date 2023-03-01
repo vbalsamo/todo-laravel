046610859ee27d7c1b8973ffe4d12ab013a075c2
vbalsamo <balvalentino@gmail.com>
2023-03-01 09:55:21 -0300

    default view

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('todos');
 });
 Route::resource('todos',  \App\Http\Controllers\TodoController::class);
 Route::get('todos/{todo}', [\App\Http\Controllers\TodoController::class,'destroy'])->name('todos.destroy');
