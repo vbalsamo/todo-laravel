04c541055c66cd192ebfc88abfbec499f5474b86
vbalsamo <balvalentino@gmail.com>
2023-03-02 11:29:01 -0300

    bootstrap cdn

--- resources/views/layouts/app-master.blade.php
+++ resources/views/layouts/app-master.blade.php
@@ -9,7 +9,7 @@
     <title>Fixed top navbar example · Bootstrap v5.1</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="{!! url('assets/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <style>
         .bd-placeholder-img {
